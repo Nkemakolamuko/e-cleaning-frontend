d5b799001d304a0fa7e41ca4265d465e508e7e08
Nkemakolauko <nwekevino99@gmail.com>
2024-04-28 19:32:05 +0100

    Fix the cart total -- phewww alas

--- src/components/modal/ProductSummary.jsx
+++ src/components/modal/ProductSummary.jsx
@@ -24,32 +24,14 @@ const ProductSummary = () => {
     darkMode,
   } = useContext(BgContext);
 
-  // const [newQty, setNewQty] = useState(1);
-
   const addedProduct = data?.find((product) => {
     return product.id == cartId;
   });
 
-  // useEffect(() => {
-  //   const calculatePrice = cartItem.map((val) => {
-  //     // Since cartItem is an array of objects, I used map to access each individual object - then I accessed the 'newPrice' and used split to remove the '₦' because I want to sum up the values and '₦' can't be converted to Number later - the split returns an array with 2 values, but the first is an empty string, so i collect only the second value at index[1] - then I split the value again because I have to remove the trailing ',' in the amount - then I used join("") without any argument so I can put all the values inside an array
-  //     return val.newPrice.split("₦")[1].split(",").join("");
-  //   });
-  //   // I assigned the value from the above iteration to totalPrice so I won't have to work with the main value itself
-  //   const totalPrice = calculatePrice;
-
-  //   // Then I used reduce method on the values - since it's an object (I used typeof to check) - so I can sum them all up, but when the summation is been done, I converted the individual values from strings to numbers ao I'd get summation and not concatenation
-  //   const sumTotal = totalPrice.reduce((pVal, cVal) => {
-  //     return Number(pVal) + Number(cVal);
-  //   }, 0); // The initial value of '0' is so it doesn't through an error when the array is empty (Probably when we deleting items from cart)
-  //   // Then of course I set the value to an existing null 'total'
-  //   setTotal(sumTotal);
-  // }, []);
-
   useEffect(() => {
     const calculatePrice = cartItem.map((val) => {
-      // Since cartItem is an array of objects, I used map to access each individual object - then I accessed the 'newPrice' and used split to remove the '₦' because I want to sum up the values and '₦' can't be converted to Number later - the split returns an array with 2 values, but the first is an empty string, so i collect only the second value at index[1] - then I split the value again because I have to remove the trailing ',' in the amount - then I used join("") without any argument so I can put all the values inside an array
-      return val.newPrice.split("₦")[1].split(",").join("");
+      // Since cartItem is an array of objects, I used map to access each individual object - then I accessed the 'newPrice' and used split to remove the '₦' because I want to sum up the values and '₦' can't be converted to Number later - the split returns an array with 2 values, but the first is an empty string, so i collect only the second value at index[1] - then I split the value again because I have to remove the trailing ',' in the amount - then I used join("") without any argument so I can put all the values inside an array and multiplied it by it's individual quantity
+      return val.newPrice.split("₦")[1].split(",").join("") * val.quantity;
     });
     // I assigned the value from the above iteration to totalPrice so I won't have to work with the main value itself
     const totalPrice = calculatePrice;
@@ -57,12 +39,29 @@ const ProductSummary = () => {
     // Then I used reduce method on the values - since it's an object (I used typeof to check) - so I can sum them all up, but when the summation is been done, I converted the individual values from strings to numbers ao I'd get summation and not concatenation
     const sumTotal = totalPrice.reduce((pVal, cVal) => {
       return Number(pVal) + Number(cVal);
-    }, 0); // The initial value of '0' is so it doesn't through an error when the array is empty (Probably when we deleting items from cart) - (this is why the total goes back to initial value before summing up again)
+    }, Number(addedProduct.newPrice.split("₦")[1].split(",").join("") - addedProduct.newPrice.split("₦")[1].split(",").join(""))); // The initial value of '0' is so it doesn't through an error when the array is empty (Probably when we deleting items from cart) - but the '0' caused some logical errors by resetting the total, so i used the price of the last added item minus itself to avoid duplicates - and voila, it worked!!
+
+    // Then of course I set the value to an existing null 'total'
+    setTotal(sumTotal);
+  }, []);
+
+  // For delete to take effect immediately
+  useEffect(() => {
+    const calculatePrice = cartItem.map((val) => {
+      // Since cartItem is an array of objects, I used map to access each individual object - then I accessed the 'newPrice' and used split to remove the '₦' because I want to sum up the values and '₦' can't be converted to Number later - the split returns an array with 2 values, but the first is an empty string, so i collect only the second value at index[1] - then I split the value again because I have to remove the trailing ',' in the amount - then I used join("") without any argument so I can put all the values inside an array and multiplied it by it's individual quantity
+      return val.newPrice.split("₦")[1].split(",").join("") * val.quantity;
+    });
+    // I assigned the value from the above iteration to totalPrice so I won't have to work with the main value itself
+    const totalPrice = calculatePrice;
+
+    // Then I used reduce method on the values - since it's an object (I used typeof to check) - so I can sum them all up, but when the summation is been done, I converted the individual values from strings to numbers ao I'd get summation and not concatenation
+    const sumTotal = totalPrice.reduce((pVal, cVal) => {
+      return Number(pVal) + Number(cVal);
+    }, Number(addedProduct.newPrice.split("₦")[1].split(",").join("") - addedProduct.newPrice.split("₦")[1].split(",").join(""))); // The initial value of '0' is so it doesn't through an error when the array is empty (Probably when we deleting items from cart) - but the '0' caused some logical errors by resetting the total, so i used the price of the last added item minus itself to avoid duplicates - and voila, it worked!!
+
     // Then of course I set the value to an existing null 'total'
     setTotal(sumTotal);
   }, [cartItem]);
-
-  // Why there are two useEffects is for when the page loads and when there's a change in the cartItem for the iteration to run again -
 
   useEffect(() => {
     if (cartItem.includes(addedProduct) || addedProduct == undefined) {
