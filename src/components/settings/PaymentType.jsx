58c2c33ba0b0eba7114a0dcbb5695a6bea2f9a62
Nkemakolauko <nwekevino99@gmail.com>
2024-05-14 17:35:40 +0100

    Create UI for Admin Cleaners page --

--- src/components/settings/PaymentType.jsx
+++ src/components/settings/PaymentType.jsx
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import { FaTimes } from "react-icons/fa";
 import CloseButton from "../CloseButton";
 import { BgContext } from "../../App";
+import { ToastContainer, toast } from "react-toastify";
 
 const PaymentType = ({ handleClose }) => {
   const { darkMode } = useContext(BgContext);
   return (
     <div className="w-full h-screen bg-slate-500/75 flex items-center justify-center absolute top-0 p-[10px] md:p-[100px] lg:p-[300px] flex-col">
+      <ToastContainer />
       <div
         className={`w-full rounded-md flex flex-col gap-2 bg-white text-center ${
           darkMode ? "dark-mode border" : ""
@@ -27,14 +29,14 @@ const PaymentType = ({ handleClose }) => {
         <div className="px-4 pb-6 pt-4 flex flex-col gap-2">
           <p
             className="w-full bg-green-500 hover:bg-green-600 text-white p-3 md:p-4 !tracking-widest md:text-lg rounded font-semibold cursor-pointer transition-all duration-300"
-            onClick={() => alert("This method is not advisable.")}
+            onClick={() => toast("This method is not advisable.")}
           >
             Cash
           </p>
           <p
             className="w-full bg-blue-500 hover:bg-blue-600 text-white p-3 md:p-4 !tracking-widest md:text-lg rounded font-semibold cursor-pointer transition-all duration-300"
             onClick={() => {
-              alert(
+              toast(
                 "You'd be redirected to our trusted payment partner shortly."
               );
             }}
--- src/pages/admin-dashboard/AdminCleaners.jsx
+++ src/pages/admin-dashboard/AdminCleaners.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useRef, useState } from "react";
 import Title from "../../components/Title";
-import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleRight, FaPen, FaTrashCan } from "react-icons/fa6";
+import img from "../../assets/download.jpg";
+import cleanersData from "../../../db/cleanersDb";
 
 const AdminCleaners = () => {
   const cleanerRef = useRef();
   const [location, setLocation] = useState(false);
+  const [cleaners, setCleaners] = useState([]);
+
   const [value, setValue] = useState("By");
   useEffect(() => {
     cleanerRef.current.focus();
+
+    const data = cleanersData;
+    setCleaners(data);
   }, []);
   return (
     <div>
       <Title title="Cleaners" />
 
-      <div
-        className="p-4 border rounded flex flex-col relative"
-        ref={cleanerRef}
-      >
-        <div className="p-2 border rounded w-full mx-auto flex justify-between items-center">
+      <div className="p-4 border rounded flex flex-col" ref={cleanerRef}>
+        <div className="p-2 border rounded w-full md:w-1/2 mx-auto flex justify-between items-center relative">
           <p className="font-medium text-sm">Registered Cleaners</p>
           <select
             name="cleaners"
@@ -26,7 +30,7 @@ const AdminCleaners = () => {
             onChange={(e) => {
               setLocation(true);
               setValue(e.target.value);
-              console.log(value);
+              // console.log(value);
             }}
           >
             <option value="By">---Filter By---</option>
@@ -35,44 +39,44 @@ const AdminCleaners = () => {
             {/* This will return a kinda search history thingy under the select that shows list of locations and a greater than arrow that when the card is clicked on, a request is made to the backend to return cleaners in that location */}
           </select>
           {location && (
-            <p className="absolute w-fit h-fit right-4 md:right-4 top-16 border rounded-b flex flex-col bg-white text-sm">
+            <p className="absolute w-fit h-fit right-2 top-10 border rounded flex flex-col bg-white text-sm">
               <span
-                className="py-1 px-[34px] flex items-center gap-2 cursor-pointer border-b hover:bg-slate-100"
+                className="py-1 px-4 flex items-center gap-[34px] text-slate-600 hover:text-slate-800 transition-all duration-300 justify-between cursor-pointer border-b hover:bg-slate-100"
                 onClick={() => setLocation(false)}
               >
                 <span>Lagos</span>
                 <FaAngleRight />
               </span>
               <span
-                className="py-1 px-[34px] flex items-center gap-2 cursor-pointer border-b hover:bg-slate-100"
+                className="py-1 px-4 flex items-center gap-[34px] text-slate-600 hover:text-slate-800 transition-all duration-300 justify-between cursor-pointer border-b hover:bg-slate-100"
                 onClick={() => setLocation(false)}
               >
                 <span>Abuja</span>
                 <FaAngleRight />
               </span>
               <span
-                className="py-1 px-[34px] flex items-center gap-2 cursor-pointer border-b hover:bg-slate-100"
+                className="py-1 px-4 flex items-center gap-[34px] text-slate-600 hover:text-slate-800 transition-all duration-300 justify-between cursor-pointer border-b hover:bg-slate-100"
                 onClick={() => setLocation(false)}
               >
                 <span>Aba</span>
                 <FaAngleRight />
               </span>
               <span
-                className="py-1 px-[34px] flex items-center gap-2 cursor-pointer border-b hover:bg-slate-100"
+                className="py-1 px-4 flex items-center gap-[34px] text-slate-600 hover:text-slate-800 transition-all duration-300 justify-between cursor-pointer border-b hover:bg-slate-100"
                 onClick={() => setLocation(false)}
               >
                 <span>Owerri</span>
                 <FaAngleRight />
               </span>
               <span
-                className="py-1 px-[34px] flex items-center gap-2 cursor-pointer border-b hover:bg-slate-100"
+                className="py-1 px-4 flex items-center gap-[34px] text-slate-600 hover:text-slate-800 transition-all duration-300 justify-between cursor-pointer border-b hover:bg-slate-100"
                 onClick={() => setLocation(false)}
               >
                 <span>Enugu</span>
                 <FaAngleRight />
               </span>
               <span
-                className="py-1 px-[34px] flex items-center gap-2 cursor-pointer border-b hover:bg-slate-100"
+                className="py-1 px-4 flex items-center gap-[34px] text-slate-600 hover:text-slate-800 transition-all duration-300 justify-between cursor-pointer border-b hover:bg-slate-100"
                 onClick={() => setLocation(false)}
               >
                 <span>Ondo</span>
@@ -80,6 +84,53 @@ const AdminCleaners = () => {
               </span>
             </p>
           )}
+        </div>
+
+        {/* Clicking on edit button only allows for editing the rating -- we not allowed to edit other info */}
+        {/* Cleaners Card */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-2">
+          {cleaners?.map((cleaner) => (
+            <div
+              className="border rounded flex flex-col w-full p-2 text-sm text-center"
+              key={cleaner.id}
+            >
+              <img
+                src={img}
+                alt="cleaner Image"
+                width={"50%"}
+                height={"50%"}
+                className="mx-auto"
+              />
+
+              <p className="py-2 font-semibold text-base">
+                {cleaner.businessName} - {cleaner.rating} star rating.
+              </p>
+              <div className="flex items-center gap-2 pb-2 w-full lg:w-1/2 justify-center mx-auto text-nowrap">
+                <code>{cleaner.phoneNumber} - </code>
+
+                <code className="">{cleaner.name}</code>
+              </div>
+              <p className="pb-2 text-slate-700">{cleaner.desc}</p>
+
+              <p className="pb-2 text-slate-700">
+                <span className="font-medium">Location:</span>{" "}
+                {cleaner.location}
+              </p>
+
+              <p className="pb-2 text-slate-700">
+                <span className="font-medium">Address:</span> {cleaner.address}
+              </p>
+
+              <div className="w-full flex flex-col md:flex-row items-center gap-2">
+                <p className="rounded text-center py-3 border border-green-500 text-green-500 active:scale-95 cursor-pointer w-full flex justify-center">
+                  <FaPen />
+                </p>
+                <p className="rounded text-center py-3 border border-rose-500 text-rose-500 active:scale-95 cursor-pointer w-full flex justify-center">
+                  <FaTrashCan />
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
